d2fece12ff50255fc640192b47bd33ebf3bb345e
unknown <myriahvielle619@gmail.com>
2025-11-29 13:46:52 +0800

    Implement Todos component

--- appdev1-final-exam/src/main.jsx
+++ appdev1-final-exam/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
